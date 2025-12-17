f78f64318d377496cf3855ed981ba3a88c744c9b
Dhemy <imdhemy@gmail.com>
2025-12-17 19:43:33 +0100

    chore(docker): expose vite ports

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,14 @@ import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 4173,
+  },
   resolve: {
     alias: {
       '@': '/src',
