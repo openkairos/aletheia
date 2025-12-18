63f65210d525eb34ca519bce557e4c72ecb16e68
Dhemy <imdhemy@gmail.com>
2025-12-18 12:37:46 +0100

    chore: remove server and preview configuration from Vite setup

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,6 @@ import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    host: '0.0.0.0',
-    port: 5173,
-  },
-  preview: {
-    host: '0.0.0.0',
-    port: 4173,
-  },
   resolve: {
     alias: {
       '@': '/src',
