33bcca24ee364473089ce22605ac475236d54191
Dhemy <imdhemy@gmail.com>
2025-12-20 15:12:57 +0100

    wip(routing): remove auth layout

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,3 @@
-import AuthLayout from '@/pages/AuthLayout.tsx';
-import DashboardLayout from '@/pages/DashboardLayout.tsx';
-import Login from '@/pages/Login.tsx';
 import { createBrowserRouter, Outlet } from 'react-router';
 
 export const router = createBrowserRouter([
@@ -8,19 +5,7 @@ export const router = createBrowserRouter([
     Component: Outlet,
     children: [
       {
-        path: 'auth',
-        Component: AuthLayout,
-        children: [
-          {
-            path: 'login',
-            Component: Login,
-          },
-        ],
-      },
-      {
         path: '/',
-        Component: DashboardLayout,
-        children: [],
       },
     ],
   },
