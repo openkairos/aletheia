19bb065751c1a68942c37035643948d1ce904c71
Dhemy <imdhemy@gmail.com>
2025-12-20 15:37:30 +0100

    chore(auth): update import path

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -1,3 +1,4 @@
 export * from './types';
 export * from './hooks/useAuth';
 export * from './contexts/AuthProvider/AuthProvider';
+export * from './login/RootRedirect/RootRedirect.tsx';
--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,4 @@
-import { RootRedirect } from '@/auth/login/RootRedirect/RootRedirect.tsx';
+import { RootRedirect } from '@/auth';
 import { createBrowserRouter, Outlet } from 'react-router';
 
 export const router = createBrowserRouter([
