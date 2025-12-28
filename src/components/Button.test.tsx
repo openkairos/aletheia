eec5d158add06fc4ba6e06bbd814cadfdfec807c
Mohamed Elshety <elshetydev99@gmail.com>
2025-12-28 19:18:33 +0200

    chore(tests): refactor button tests to follow aaa pattern

--- src/components/Button.test.tsx
+++ src/components/Button.test.tsx
@@ -13,8 +13,8 @@ test('it should render children and default type', () => {
 
 test('it should call onClick prop when clicked', async () => {
   const handleClick = vi.fn();
-
   render(<Button onClick={handleClick}>Click Me</Button>);
+
   await userEvent.click(screen.getByText(/click me/i));
 
   expect(handleClick).toHaveBeenCalledTimes(1);
@@ -22,12 +22,12 @@ test('it should call onClick prop when clicked', async () => {
 
 test('it should not call onClick when button is disabled', async () => {
   const handleClick = vi.fn();
-
   render(
     <Button onClick={handleClick} disabled>
       Click me{' '}
     </Button>,
   );
+
   await userEvent.click(screen.getByRole('button'));
 
   expect(handleClick).not.toHaveBeenCalled();
@@ -36,8 +36,8 @@ test('it should not call onClick when button is disabled', async () => {
 test('it should render the correct button type', () => {
   const types = ['button', 'submit', 'reset'] as const;
   const randomType = types[Math.floor(Math.random() * types.length)];
-
   render(<Button type={randomType}>Click me</Button>);
+
   const button = screen.getByRole('button', { name: /click me/i });
 
   expect(button).toHaveAttribute('type', randomType);
