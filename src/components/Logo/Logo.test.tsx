85fa92c399a30e4375315a6fffbc8dd7342f03ea
Mohamed Elshety <elshetydev99@gmail.com>
2025-12-31 15:00:57 +0200

    chore(ds): add test for logo renders without dimensions by default

--- src/components/Logo/Logo.test.tsx
+++ src/components/Logo/Logo.test.tsx
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import { Logo } from './Logo';
+import { expect } from 'vitest';
 
 test('it should apply custom width and height props', () => {
   const width = 200;
@@ -10,4 +11,13 @@ test('it should apply custom width and height props', () => {
 
   expect(logoImage).toHaveAttribute('width', String(width));
   expect(logoImage).toHaveAttribute('height', String(height));
+});
+
+test('it should render component without custom width and height props', () => {
+  render(<Logo />);
+
+  const logoImage = screen.getByRole('img', { name: /aletheia/i });
+
+  expect(logoImage).not.toHaveAttribute('width');
+  expect(logoImage).not.toHaveAttribute('height');
 });
